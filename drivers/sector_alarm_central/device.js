0fdf5dd1a6d4b4160d3a0ecb4b2ec973abfde137
Vaggan <christopher.edling@gmaiil.com>
2020-10-10 12:49:32 +0200

    Rewrite onCapabilityChanged and some logging

--- drivers/sector_alarm_central/device.js
+++ drivers/sector_alarm_central/device.js
@@ -33,18 +33,18 @@ class MyDevice extends Homey.Device {
         this._site = site;
       })
       .catch(error => {
-        this.log(`Error: ${error}`);
+        this.error(error);
       });
     try {
       this._pollAlarmInterval = setInterval(this.pollAlarmStatus.bind(this), POLL_INTERVAL);
-    } catch (e) {
-      this.log(e.message);
+    } catch (error) {
+      this.error(error);
     }
 
     await this.setInitState();
     this.registerCapabilityListener('homealarm_state', this.onCapabilityChanged.bind(this));
-    await this.registerFlowCardCondition();
-    await this.registerFlowCardAction();
+    this.registerFlowCardCondition();
+    this.registerFlowCardAction();
   }
 
   async setInitState() {
@@ -61,7 +61,7 @@ class MyDevice extends Homey.Device {
         this.setCapabilityValue('homealarm_state', armedState);
       })
       .catch(error => {
-        this.log(`Error: ${error}`);
+        this.error(error);
       });
   }
 
@@ -79,7 +79,7 @@ class MyDevice extends Homey.Device {
             }
           })
           .catch(error => {
-            this.log(`Error: ${error}`);
+            this.error(error);
           });
       });
   }
@@ -110,7 +110,7 @@ class MyDevice extends Homey.Device {
       this._site.status()
         .then(async status => {
           this.onAlarmUpdate(status);
-          Promise.resolve().catch(this.log);
+          Promise.resolve().catch(this.error);
         })
         .catch(error => {
           if (error.code === 'ERR_INVALID_SESSION') {
@@ -118,11 +118,11 @@ class MyDevice extends Homey.Device {
             this._site.login()
               .then(() => this.pollAlarmStatus());
           } else {
-            this.error(`Error: ${error}`);
+            this.error(error);
           }
         });
-    } catch (e) {
-      this.error(e);
+    } catch (error) {
+      this.error(error);
     }
   }
 
@@ -138,11 +138,12 @@ class MyDevice extends Homey.Device {
           break;
       }
       if (armedState !== this.getCapabilityValue('homealarm_state')) {
-        this.setCapabilityValue('homealarm_state', armedState);
-        this.triggerFlow(armedState);
+        this.setCapabilityValue('homealarm_state', armedState).then(() => {
+          this.triggerFlow(armedState);
+        });
       }
-    } catch (e) {
-      this.log(e.message);
+    } catch (error) {
+      this.error(error);
     }
   }
 
@@ -152,48 +153,92 @@ class MyDevice extends Homey.Device {
       const stateChangedTrigger = this.homey.flow.getTriggerCard('homealarm_state_changed');
       stateChangedTrigger
         .trigger(null, state)
-        .then(this.log(`homealarm_state_changed trigger: ${triggeredState}`))
+        .then(this.log(`Flow homealarm_state_changed triggered whith state: ${triggeredState}`))
         .catch(error => {
-          this.log(`Error: ${error}`);
+          this.error(error);
         });
-    } catch (e) {
-      this.log(e.message);
+    } catch (error) {
+      this.error(error);
     }
   }
 
   onAdded() {
     try {
       this.pollAlarmStatus();
-    } catch (e) {
-      this.log(e.message);
+    } catch (error) {
+      this.error(error);
     }
   }
 
-  onCapabilityChanged(value, opts) {
-    const code = this.homey.settings.get(settings.CODE);
-    if (code === '') {
-      Promise.reject();
-    }
-    switch (value) {
-      case ALARMSTATE.ARMED:
-        this._site.arm(code).catch(error => {
-          this.log(`Error: ${error.code} Message: ${error.message}`);
+  async onCapabilityChanged(value, opts) {
+    return new Promise((resolve, reject) => {
+      switch (value) {
+        case ALARMSTATE.ARMED:
+          this.setAlarmState(ALARMSTATE.ARMED, this._site.arm.bind(this._site))
+            .then(message => {
+              this.log(message);
+              resolve();
+            })
+            .catch(error => {
+              this.error(error);
+              reject(error);
+            });
+          break;
+        case ALARMSTATE.DISARMED:
+          this.setAlarmState(ALARMSTATE.DISARMED, this._site.disarm.bind(this._site))
+            .then(message => {
+              this.log(message);
+              resolve();
+            })
+            .catch(error => {
+              this.error(error);
+              reject(error);
+            });
+          break;
+        case ALARMSTATE.PARTIALYARMED.HOMEY:
+          // eslint-disable-next-line max-len
+          this.setAlarmState(ALARMSTATE.PARTIALYARMED.HOMEY, this._site.partialArm.bind(this._site))
+            .then(message => {
+              this.log(message);
+              resolve();
+            })
+            .catch(error => {
+              this.error(error);
+              reject(error);
+            });
+          break;
+        default:
+          reject(new Error('Failed to set alarm state.'));
+          break;
+      }
+    });
+  }
+
+  async setAlarmState(state, action) {
+    return new Promise((resolve, reject) => {
+      const code = this.homey.settings.get(settings.CODE);
+      if (code === '') {
+        reject(new Error('No alarm code set'));
+      }
+
+      const sleep = m => new Promise(r => setTimeout(r, m));
+
+      action(code)
+        .then(() => {
+          resolve(`Successfully set the alarm to: ${state}`);
+        })
+        .catch(error => {
+          sleep(5000).then(() => {
+            action(code)
+              .then(() => {
+                resolve(`Successfully set the alarm to: ${state}`);
+              })
+              .catch(() => {
+                reject(new Error(`Faild to set the alarm to: ${state}`));
+              });
+          });
         });
-        break;
-      case ALARMSTATE.DISARMED:
-        this._site.disarm(code).catch(error => {
-          this.log(`Error: ${error.code} Message: ${error.message}`);
-        });
-        break;
-      case ALARMSTATE.PARTIALYARMED.HOMEY:
-        this._site.partialArm(code).catch(error => {
-          this.log(`Error: ${error.code} Message: ${error.message}`);
-        });
-        break;
-      default:
-        break;
-    }
-    Promise.resolve();
+    });
   }
 
 }
