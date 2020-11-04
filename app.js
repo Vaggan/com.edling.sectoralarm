cb3a5123559310188486f24b7eebed29e17e7bea
Vaggan <christopher.edling@gmaiil.com>
2020-11-04 21:48:02 +0100

    Enable debugging

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+/* eslint-disable node/no-unsupported-features/node-builtins */
+/* eslint-disable global-require */
+
 'use strict';
 
 const Homey = require('homey');
+
+if (process.env.DEBUG === '1') {
+  require('inspector').open(9229, '0.0.0.0', false);
+}
 
 class MyApp extends Homey.App {
 
