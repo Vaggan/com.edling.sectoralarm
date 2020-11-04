2aa10274aaa39f7d2ddc67469e91e587c52c10a1
Vaggan <christopher.edling@gmaiil.com>
2020-11-04 22:12:21 +0100

    Add homey-log

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ if (process.env.DEBUG === '1') {
   require('inspector').open(9229, '0.0.0.0', false);
 }
 
+const { Log } = require('homey-log');
+
 class MyApp extends Homey.App {
 
   onInit() {
     this.log('MyApp is running...');
+    this.homeyLog = new Log({ homey: this.homey });
   }
 
 }
