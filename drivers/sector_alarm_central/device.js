d95ac2dba27227fdd3d0b98d0aacffde3da1233b
Jonas Inghammar <jonas.inghammar@effectsoft.se>
2021-05-06 00:37:11 +0200

    Fixed error when arming or disarming
    Arming or disarming gave an error for 'No alarm code set' due to incorrect if statement

--- drivers/sector_alarm_central/device.js
+++ drivers/sector_alarm_central/device.js
@@ -316,7 +316,7 @@ class MyDevice extends Homey.Device {
   async setAlarmState(state, action) {
     this.homey.app.updateLog('Function setAlarmState start', 2);
     return new Promise((resolve, reject) => {
-      if (code || code === '') {
+      if (code === '') {
         reject(new Error('No alarm code set'));
       }
 
